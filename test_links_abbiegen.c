34066b78426ab7bd1419dacba63f4a1be9f873f1
ies <ies@ies-labor>
2024-06-14 11:05:30 +0200

    added all the files from yesterdays lab work

---
+++ test_links_abbiegen.c
@@ -0,0 +1,35 @@
+#include "iesmotors.h"
+#include <util/delay.h>
+
+
+int main(void) {
+	// Delete everything on ports B and D
+    DDRD = 0;
+    DDRB = 0;
+
+    // Set PD5 and PD6 as output (EN[A|B]!)
+    DDRD = (1 << DD5) | (1 << DD6);
+
+    // Set PD7 as output (IN1)
+    DDRD |= (1 << DD7);
+
+    // Make PWM work on PD[5|6], immer fue motor
+    setupTimer0();
+
+    // Set PB0, PB1, and PB3 as output (IN[2|3|4])
+    DDRB = (1 << DD0) | (1 << DD1) | (1 << DD3);
+
+    // Set the duty cycles for PD5/PD6
+    setDutyCycle(PD5, 155);
+    setDutyCycle(PD6, 155);
+    
+    while(1){
+    // Left motors forward, starting with PWM=0% and slowly increas the duty
+    setDutyCycle(PD5, 155);
+    PORTD |= (1 << PB7);
+
+
+    return 0;
+		
+		}
+	}
