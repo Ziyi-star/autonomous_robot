4a0afe38557faff2d1e6236dd3b9239f2e96c1d2
ies <ies@ies-labor>
2024-06-27 11:37:35 +0200

    Added file with makros for more reliability.

--- test_line.c
+++ test_line.c
@@ -8,15 +8,16 @@ void turn_right(){
 	// Set the duty cycles for PD5/PD6
     setDutyCycle(PD5, 200);
     setDutyCycle(PD6, 200);
-	// links rückwärts
+    // Set IN2 to LOW, nothing else HIGH -> No motors turn
     PORTB &= ~(1 << PB0);
-	//Right motors BACKWARD
+    // Set IN3 to HIGH and don't set anything else to HIGH -> Right motors BACKWARD
     PORTB |= (1 << PB1);
-
+    // Set IN4 to LOW, nothing else HIGH -> No motors turn
 	PORTB &= ~(1 << PB3);
-	//links Motor rückwärt
-	PORTD |= (1 << PD7);
+	// Set IN1 to HIGH and don't set IN2 to HIGH (leave LOW) -> Left motors FORWARD
+    PORTD |= (1 << PD7); // Use OR, since overwriting will disable EN[A|B]!
 		}
+		
 //leichte abbiegen
 void turn_left(){
 	    // Set the duty cycles for PD5/PD6
@@ -36,13 +37,12 @@ void turn_left(){
 
 
 void drive_right(){
-	    // Set the duty cycles for PD5/PD6
+	// Set the duty cycles for PD5/PD6
     setDutyCycle(PD5, 155);
     setDutyCycle(PD6, 155);
 	// Both sides stop, backword
     PORTB &= ~(1 << PB0);
     PORTB &= ~(1 << PB1);
-	
 	// Left motors forward, starting with PWM=0% and slowly increas the duty
 	setDutyCycle(PD5, 155);
 	PORTD |= (1 << PB7);
@@ -66,13 +66,12 @@ void drive_left(){
 	}
 
 void gerade(){
-	// Set the duty cycles for PD5/PD6
+	// Set the duty cycles for PD5/PD6,speed
     setDutyCycle(PD5, 155);
     setDutyCycle(PD6, 155);
 	// Both sides stop, backword
     PORTB &= ~(1 << PB0);
     PORTB &= ~(1 << PB1);
-
 	// Both sides forward
 	PORTD |= (1 << PD7);
 	PORTB |= (1 << PB3);
@@ -81,26 +80,19 @@ void gerade(){
 void init_run(){
 	// Set Data Direction Register C [0|1|2| as input.
     DDRC = ~((1<<DDC0) | (1<<DDC1) | (1<<DDC2));
-    
     // Initialze U(S)ART!
     USART_init(UBRR_SETTING);
-    
     // Delete everything on ports B and D
     DDRD = 0;
     DDRB = 0;
-
     // Set PD5 and PD6 as output (EN[A|B]!)
     DDRD = (1 << DD5) | (1 << DD6);
-
     // Set PD7 as output (IN1)
     DDRD |= (1 << DD7);
-
-    // Make PWM work on PD[5|6], immer fue motor
+    // Make PWM work on PD[5|6], immer fur motor
     setupTimer0();
-
     // Set PB0, PB1, and PB3 as output (IN[2|3|4])
     DDRB = (1 << DD0) | (1 << DD1) | (1 << DD3);
-
     // Set the duty cycles for PD5/PD6
     setDutyCycle(PD5, 155);
     setDutyCycle(PD6, 155);
@@ -132,8 +124,6 @@ int main(void) {
         adcval0 = ADC_read_avg(ADMUX_CHN_ADC0, ADC_AVG_WINDOW);
         adcval1 = ADC_read_avg(ADMUX_CHN_ADC1, ADC_AVG_WINDOW);
         adcval2 = ADC_read_avg(ADMUX_CHN_ADC2, ADC_AVG_WINDOW);
-
-        
         //MIDDLE
         if (adcval1>280) {
            gerade();
---
+++ test_line_rename.c
@@ -0,0 +1,4 @@
+#include <avr/io.h>
+#include "iesusart.h"
+#include "iesmotors.h"
+#include "iesadc.h"
