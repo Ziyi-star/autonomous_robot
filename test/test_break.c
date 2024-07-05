de78bacb7fab2ab332a951f73e1b3d64db77423c
ies <ies@ies-labor>
2024-07-05 23:19:15 +0200

    add test_break file.

---
+++ test/test_break.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int m = 10;
+    while (1) {
+        m--;
+        printf("%d", m);
+        if (m == 0) {
+            break;
+        }
+
+    }
+
+}
+
